8c9f681f53487cfafc536839b81a6abd5ccea7cc
zeros <thaicobra1610@gmail.com>
2025-10-27 01:16:42 +0700

    push for review

--- backend/scripts/load_recipe_to_vector_store.py
+++ backend/scripts/load_recipe_to_vector_store.py
@@ -1,12 +1,14 @@
 import asyncio
 import sys
+import uuid
 from asyncio import sleep
 from pathlib import Path
 from langchain_core.documents import Document
 from langchain_text_splitters.markdown import MarkdownHeaderTextSplitter
 from tqdm.asyncio import tqdm
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient, AsyncQdrantClient
+from qdrant_client.http.models import VectorParams, Distance
 
 # Add the project root to the path
 sys.path.insert(0, str(Path(__file__).parent.parent))
@@ -42,6 +44,55 @@ async def process_recipe(recipe: Recipe):
     ]
 
     splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+
+    chunks = []
+    split_docs = splitter.split_text(document.page_content)
+    for split_doc in split_docs:
+        chunks.append(
+            Document(
+                page_content=split_doc.page_content,
+                metadata={**split_doc.metadata, **document.metadata},
+            )
+        )
+
+    embedding_model = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key='AIzaSyAidudi2tMz-UEDfguhOZx7Xcp7xJaK-Zs')
+    qdrant_client = AsyncQdrantClient(url=settings.QDRANT_URL)
+
+    collections = await qdrant_client.get_collections()
+    collection_names = [collection.name for collection in collections.collections]
+
+    if settings.QDRANT_RECIPE_COLLECTION not in collection_names:
+        vector_size = 768
+        await qdrant_client.create_collection(
+            collection_name=settings.QDRANT_RECIPE_COLLECTION,
+            vectors_config=VectorParams(
+                size=vector_size,
+                distance=Distance.COSINE
+            )
+        )
+    else:
+        await qdrant_client.delete_collection(collection_name=settings.QDRANT_RECIPE_COLLECTION)
+        vector_size = 768
+        await qdrant_client.create_collection(
+            collection_name=settings.QDRANT_RECIPE_COLLECTION,
+            vectors_config=VectorParams(
+                size=vector_size,
+                distance=Distance.COSINE
+            )
+        )
+
+    vector_store = QdrantVectorStore(
+        client=qdrant_client,
+        collection_name=settings.QDRANT_RECIPE_COLLECTION,
+        embedding=embedding_model,
+    )
+
+    chunks_ids = [str(uuid.uuid4()) for _ in range(len(chunks))]
+
+    vector_store.add_documents(
+        documents=chunks,
+        ids=chunks_ids
+    )
 
     await sleep(0.001)  # Simulate some processing time
     # print(f"Processing recipe: {recipe.title} (ID: {recipe.id})")
